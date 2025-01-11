8c67e1764f68042d3a25bca42f4a7a5b711625b7
Caio Reidt <caio@caioreidt.dev>
2025-01-11 16:07:58 -0300

    Set login as home page

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', '/login');
 
 Route::middleware([
-    'auth:sanctum',
+    'auth:sanctum', 'verified',
     config('jetstream.auth_session'),
-    'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
